a33745fc3ea557d400baa10050c0c97184869803
Arthur de oliveira silva <arthuroli258@gmail.com>
2026-03-30 10:30:48 -0300

    Update pilha3003.cpp

--- pilha3003.cpp
+++ pilha3003.cpp
@@ -21,7 +21,10 @@ int main()
 		cout<<"\n2- Remover um valor da pilha";
 		cout<<"\n3- Mostrar o elemento do topo da pilha";
 		cout<<"\n4- Mostrar situacao da pilha";
-		cout<<"\n5- Sair";
+		cout<<"\n5- Mostrar maior elemento da Pilha";
+		cout<<"\n6- Mostrar elementos pares da Pilha"; 
+		cout<<"\n7- Mostrar elementos impares da Pilha"; 
+		cout<<"\n8- Sai";
 		cout<<"\nOpcao: ";
 		cin>>op;
 		system("cls");
@@ -52,8 +55,62 @@ int main()
 				situacaoPilha(pilha, topo);
 	            break;
 
-			case 5: 
-				cout<<"\nPrograma basico de PILHA\n";	
+			case 5: {
+				int maior;
+				if(topo == -1){
+					cout<<"\nATENCAO. Pilha Vazia\n";
+				}else{
+					maior = pilha[0];
+					for(int i = 1; i <= topo; i++){
+						if(pilha[i] > maior){
+							maior = pilha[i];
+						}
+					}
+					cout<<"\nMaior elemento: "<<maior;
+				}
+				break;
+			}
+
+			case 6: {
+				if(topo == -1){
+					cout<<"\nATENCAO. Pilha Vazia\n";
+				}else{
+					bool encontrou = false;
+					cout<<"\nElementos pares: ";
+					for(int i = 0; i <= topo; i++){
+						if(pilha[i] % 2 == 0){
+							cout<<pilha[i]<<" ";
+							encontrou = true;
+						}
+					}
+					if(!encontrou){
+						cout<<"Nenhum elemento par encontrado";
+					}
+				}
+				break;
+			}
+
+			case 7: {
+				if(topo == -1){
+					cout<<"\nATENCAO. Pilha Vazia\n";
+				}else{
+					bool encontrou = false;
+					cout<<"\nElementos impares: ";
+					for(int i = 0; i <= topo; i++){
+						if(pilha[i] % 2 != 0){
+							cout<<pilha[i]<<" ";
+							encontrou = true;
+						}
+					}
+					if(!encontrou){
+						cout<<"Nenhum elemento impar encontrado";
+					}
+				}
+				break;
+			}
+
+			case 8: 
+				cout<<"\nSaindo do programa\n";	
 	  		    break;	
 
 			default: 
@@ -63,7 +120,7 @@ int main()
 		cout<<"\n\n";
 		system("pause");
 
-	}while(op != 5);
+	}while(op != 8);
 }
 
 /* insere*/
